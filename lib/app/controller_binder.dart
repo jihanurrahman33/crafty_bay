56a6a53daface71ca68b1e33e5d6784a34e82826
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-07-13 22:57:31 +0600

    add network client

--- lib/app/controller_binder.dart
+++ lib/app/controller_binder.dart
@@ -1,10 +1,25 @@
+import 'package:crafty_bay/core/services/network/network_client.dart';
 import 'package:crafty_bay/features/common/ui/controllers/main_bottom_nav_controller.dart';
 import 'package:get/get.dart';
 
-class ControllerBinder extends Bindings{
+class ControllerBinder extends Bindings {
   @override
   void dependencies() {
     Get.put(MainBottomNavController());
+    Get.put(
+      NetworkClient(
+        onUnauthorize: _onUnAuthorize,
+        commonHeaders: _commonHeaders,
+      ),
+    );
   }
 
+  void _onUnAuthorize() {
+    // TODO: Logout from app and re-login
+  }
+
+  final Map<String, String> _commonHeaders = {
+    'content-type': 'application/json',
+    'token': '',
+  };
 }
